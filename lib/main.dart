d033486df99589fbe565ab8c6f4ec24ef3d6eb07
W0LF12AM <bayui4742@gmail.com>
2025-05-25 22:00:10 +0700

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,42 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void playSound(int noteNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$noteNumber.wav'));
+  }
+
+  Expanded noteButton({required Color colors, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: colors, shape: LinearBorder()),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(),
+      ),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-            child: Center(
-          child: TextButton(
-              onPressed: () async {
-                try {
-                  final player = AudioPlayer();
-                  await player.play(AssetSource('note1.wav'));
-                } catch (e) {
-                  print('Error saat memainkan audio: $e');
-                }
-              },
-              child: Text('Click Here')),
+            child: Column(
+          children: [
+            noteButton(colors: Colors.red, soundNumber: 1),
+            noteButton(colors: Colors.orange, soundNumber: 2),
+            noteButton(colors: Colors.yellow, soundNumber: 3),
+            noteButton(colors: Colors.green, soundNumber: 4),
+            noteButton(colors: Colors.teal, soundNumber: 5),
+            noteButton(colors: Colors.blue, soundNumber: 6),
+            noteButton(colors: Colors.purple, soundNumber: 7),
+          ],
         )),
       ),
     );
